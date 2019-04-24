b99b7b77e37170cfc4ae7ce72dbdc137dc02452a
Frederick Morin <frederickmorin18@gmail.com>
2019-04-24 14:09:41 +1200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,65 @@
+const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+const palettes = require('nice-color-palettes');
+
+const settings = {
+  dimensions: [2048, 2048],
+};
+
+const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6);
+  const palette = random.shuffle(random.pick(palettes).slice(0, colorCount));
+
+  const createGrid = () => {
+    const points = [];
+    const count = 50;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = x / (count - 1);
+        const v = y / (count - 1);
+
+        const radius = Math.abs(random.noise2D(u, v) * 0.025);
+
+        points.push({
+          position: [u, v],
+          radius,
+          color: random.pick(palette),
+          rotation: random.noise2D(u, v),
+        });
+      }
+    }
+
+    return points;
+  };
+
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(({ position: [u, v], radius, color, rotation }) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
+      context.fillStyle = color;
+      context.font = `${radius * width * 2}px "monospace"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('\\\\', 0, 0);
+
+      context.restore();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
