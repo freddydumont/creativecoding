f62c00e9accf380e22b6e2a4490ec5f9de649579
Frederick Morin <frederickmorin18@gmail.com>
2019-04-24 22:37:30 +1200

    [sketch.js] 2019.04.24-22.37.30

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ const settings = {
 };
 
 const sketch = () => {
-  const colorCount = random.rangeFloor(1, 6);
+  const colorCount = random.rangeFloor(2, 6);
   const palette = random.shuffle(random.pick(palettes).slice(0, colorCount));
 
   const createGrid = () => {
@@ -39,8 +39,23 @@ const sketch = () => {
   return ({ context, width, height }) => {
     const margin = width * 0.175;
 
+    // reusable lerp functions
     const lerpWidth = (x) => lerp(margin, width - margin, x);
     const lerpHeight = (y) => lerp(margin, height - margin, y);
+
+    /** Draw and fill a trapezoid */
+    function drawTrapezoid([x1, y1], [x2, y2], color) {
+      context.lineWidth = 4;
+      context.beginPath();
+      context.moveTo(x1, y1);
+      context.lineTo(x2, y2);
+      context.lineTo(x2, lerpWidth(1));
+      context.lineTo(x1, lerpWidth(1));
+      context.lineTo(x1, y1);
+      context.fillStyle = color;
+      context.fill();
+      context.stroke();
+    }
 
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
@@ -63,34 +78,42 @@ const sketch = () => {
 
     const shuffled = random.shuffle(lerpedPoints);
 
+    const averaged = [];
     // do the following until there is no more points
     while (shuffled.length > 0) {
       // connect two random points on the grid
       //  - get two different random positions
-      const [
-        {
+      const [point1, point2] = shuffled.splice(0, 2);
+
+      const {
+        position: [x1, y1],
+        color,
+      } = point1;
+
+      const {
+        position: [x2, y2],
+      } = point2;
+
+      const averageY = (y1 + y2) / 2;
+
+      averaged.push({ point1, point2, averageY });
+    }
+
+    // sort the averaged array
+    averaged.sort(({ averageY: a }, { averageY: b }) => a - b);
+
+    // draw
+    averaged.forEach(
+      ({
+        point1: {
           position: [x1, y1],
           color,
         },
-        {
+        point2: {
           position: [x2, y2],
         },
-      ] = shuffled.splice(0, 2);
-
-      // draw and fill a trapezoid
-      context.lineWidth = 6;
-      context.beginPath();
-      context.moveTo(x1, y1);
-      context.lineTo(x2, y2);
-      context.lineTo(x2, lerpWidth(1));
-      context.lineTo(x1, lerpWidth(1));
-      context.lineTo(x1, y1);
-      context.fillStyle = color;
-      context.fill();
-      context.stroke();
-
-      const averageY = (y1 + y2) / 2;
-    }
+      }) => drawTrapezoid([x1, y1], [x2, y2], color)
+    );
   };
 };
 
